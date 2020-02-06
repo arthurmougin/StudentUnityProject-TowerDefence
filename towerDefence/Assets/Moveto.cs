b84575dd1ee945c79c5a193e6ca4f00ac1e788d4
wellandr <arthur.mougin@live.fr>
2020-02-06 19:19:50 +0100

    avancement safe ?

---
+++ towerDefence/Assets/Moveto.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Moveto : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject goal;
+
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        if (agent)
+        {
+            agent.destination = goal.transform.position;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
