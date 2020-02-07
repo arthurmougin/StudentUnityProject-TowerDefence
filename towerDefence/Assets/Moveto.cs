e12f4602f4acdd00f115ec7daffe4de448c26c1b
wellandr <arthur.mougin@live.fr>
2020-02-07 19:08:30 +0100

    bug de position non résolu

--- towerDefence/Assets/Moveto.cs
+++ towerDefence/Assets/Moveto.cs
@@ -7,22 +7,60 @@ public class Moveto : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public GameObject goal;
+    private GameObject goal;
+    private GameObject LocalPositionner;
+    private NavMeshAgent navAgent;
+    private float pathLength = 0f;
 
+    public float mediumLeapHeight = 0f;
+    public float maxDeltaLeapHeight = 10f;
+
+    private float LeapHeight = 0f;
+
+    public float localLeapHeight = 0f;
 
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        if (agent)
-        {
-            agent.destination = goal.transform.position;
 
-        }
+        /* Setup navigation */
+        navAgent = GetComponent<NavMeshAgent>();
+        goal = GameObject.Find("navmesh_holder/navmesh_Goal");
+        navAgent.destination = goal.transform.position;
+
+        /* Mise en place de la trajectoire courbée sur l'axe y */
+        LocalPositionner = this.gameObject.transform.GetChild(0).gameObject;
+        //on cherche aléatoirement la taille de la courbe dans l'interval donné en paramêtre
+        LeapHeight = Random.Range((mediumLeapHeight - maxDeltaLeapHeight), (mediumLeapHeight + maxDeltaLeapHeight));
+        localLeapHeight = LeapHeight;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        // gestion de fin de parcour
+        if (navAgent.remainingDistance < 5 && navAgent.remainingDistance > 0)
+        {
+            //TODO remove points to goal
+            Destroy(gameObject);
+        }
+        else
+        { // gestion de la variation d'altitude 
+
+
+            if (pathLength == 0 || pathLength == Mathf.Infinity)// Le calcul du path prend un peu de temps, on l'attend donc pour éviter une catastrophe
+            {
+                if(navAgent.remainingDistance != 0)
+                    pathLength = navAgent.remainingDistance;
+                return;
+            }
+
+
+
+            Debug.Log(localLeapHeight);
+            LocalPositionner.transform.position.Set(0, localLeapHeight, 0);
+        }
     }
 }
