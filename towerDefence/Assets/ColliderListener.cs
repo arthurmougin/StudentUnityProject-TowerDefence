2fb75a2e048de77235fa2361349a162967ab4a21
wellandr <arthur.mougin@live.fr>
2020-02-13 17:07:24 +0100

    avancement click

---
+++ towerDefence/Assets/ColliderListener.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderListener : MonoBehaviour
+{
+    public bool aviable = true;
+    private Material safe;
+    [SerializeField]
+    private Material warning;
+    private Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+        Debug.Log(rend != null);
+        safe = rend.material;
+        aviable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.name == "navmesh")
+        {
+            aviable = true;
+            rend.material = safe;
+            return;
+        }
+            
+        Debug.Log("triggerEnter");
+        aviable = false;
+        rend.material = warning;
+    }
+    void OnTriggerExit(Collider c)
+    {
+        Debug.Log("OnTriggerExit");
+        aviable = true;
+        rend.material = safe;
+    }
+    /*
+    void OnTriggerStay(Collider c)
+    {
+       // Debug.Log("OnTriggerStay");
+    }*/
+}
--- towerDefence/Assets/IslandsManager.cs
+++ towerDefence/Assets/IslandsManager.cs
@@ -8,6 +8,7 @@ public class IslandsManager : MonoBehaviour
     private bool lookingToAddIsland = false;
     public GameObject CollisionBox;
     private bool activeCollBox = false;
+    public GameObject[] island_prefabs;
 
 
     // Start is called before the first frame update
@@ -20,20 +21,29 @@ public class IslandsManager : MonoBehaviour
     void Update()
     {
         //On regarde ou pointe le joueur tout le temps
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
 
         //S'il doit ajouter une tour
         if (lookingToAddIsland)
         {
-           
-            NavMeshHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            RaycastHit  hit;
             //Alors on regarde ou il pointe sur le navmesh, s'il pointe sur le navmesh
-            if (NavMesh.Raycast(ray.origin, ray.GetPoint(100000), out hit, NavMesh.AllAreas))
+            if (Physics.Raycast(ray,out hit))
             {
                 CollisionBox.SetActive(true);
                 activeCollBox = true;
-                CollisionBox.transform.position = hit.position;
+                CollisionBox.transform.position = hit.point;
+                // Debug.Log(hit.collider.gameObject.name);
 
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GameObject babyIsland = island_prefabs[Random.Range(0, island_prefabs.Length - 1)];
+                    Vector3 rotationRand = new Vector3(0, Random.Range(0, 360), 0);
+                    Instantiate(babyIsland, hit.point,Quaternion.Euler(rotationRand),transform);
+                    onAddIsland();
+                }
             }
             else
             {
@@ -45,6 +55,16 @@ public class IslandsManager : MonoBehaviour
 
     public void onAddIsland()
     {
-        lookingToAddIsland = true;
+        lookingToAddIsland = !lookingToAddIsland;
+        if (!lookingToAddIsland)
+        {
+            CollisionBox.SetActive(false);
+            activeCollBox = false;
+        }
+        else
+        {
+            CollisionBox.SetActive(true);
+            activeCollBox = true;
+        }
     }
 }
